32ff647e4fa7a1ec73f8793c228629b715a5d13d
EduardAlex <edyalex.david@gmail.com>
2021-05-11 10:20:55 +0300

    no more crashing

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def sex():
 	rez = "scrie numele cu virgula intre ele si dupa apasa pe buton"
+	b = ""
 	if freqs.method == "POST":
+		b = freqs.form["nume"]
 		lista = freqs.form["nume"].split(",")
 		a = reqs.get("https://www.random.org/integers/", {"num" : 1, "min" : 1, "max" : len(lista), "col" : 1, "base" : 10, "format" : "plain", "rnd" : "new"}).text
 		a = a[:-1]
@@ -17,6 +19,6 @@ def sex():
 			rez = lista[int(a)-1]
 		except ValueError:
 			rez = 'serverele <a href="http://random.org/">random.org</a> sunt ocupate acum, inceacra mai tarziu geiul;e'
-	return render_template("index.html", rz = rez, inpt = freqs.form["nume"], unescape = html.unescape)
+	return render_template("index.html", rz = rez, inpt = b, unescape = html.unescape)
 
-app.run(port = 5000, threaded = True)
+app.run(port=80)
